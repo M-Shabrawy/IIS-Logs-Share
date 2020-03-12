75b4c020bd2b085825c8c506d3ca639f75b18fef
Mohamed Al-Shabrawy <12400622+M-Shabrawy@users.noreply.github.com>
2020-03-12 17:37:57 +0300

    Add files via upload

---
+++ IIS_LogSource_CSV.ps1
@@ -0,0 +1,73 @@
+﻿$MsgSourceTypeID = (Invoke-Sqlcmd -ServerInstance 'localhost' -Database LogRhythmEMDB -Query "SELECT MsgSourceTypeID FROM MsgSourceType WHERE Name = 'Flat File - Microsoft IIS W3C File'").MsgSourceTypeID
+$MsgSourceDateFormatID = (Invoke-Sqlcmd -ServerInstance 'localhost' -Database LogRhythmEMDB -Query "SELECT MsgSourceDateFormatID FROM MsgSourceDateFormat WHERE Name = 'Microsoft IIS W3C File'").MsgSourceDateFormatID
+
+$servers = Import-Csv -Path 'C:\LogRhythm\ART\IIS.csv'
+"Count: $($servers.Count)"
+
+
+Foreach($server in $servers){
+    #$server = $servers[0]
+    if ($server.Site -eq 'Default Web Site'){
+        $msgSourceName = "$($server.Name) MS IIS W3C Log"
+    }
+    else{
+        $msgSourceName = "$($server.Name) $($server.Site) IIS W3C Log"
+    }
+    $HostID = (Invoke-Sqlcmd -ServerInstance 'localhost' -Database LogRhythmEMDB -Query "SELECT HostID FROM Host WHERE Name = '$($server.Name)'" ).HostID
+    $Query = "
+    DECLARE	@return_value int,
+		@MsgSourceID int
+    EXEC	@return_value = LogRhythm_EMDB_MsgSource_Insert
+		@SystemMonitorID = 2,
+		@HostID = $($HostID),
+		@MsgSourceTypeID = 84,
+		@Name = N'$($msgSourceName)',
+		@ShortDesc = '',
+		@LongDesc = '',
+		@MsgSourceDateFormatID = 4,
+		@CollectionDepth = -1,
+		@MsgsPerCycle = 100,
+		@FilePath = N'$($server.Share)',
+		@MonitorStart = '',
+		@MonitorStop = '',
+		@DefMsgTTL = 0,
+		@DefMsgArchiveMode = 1,
+		@MPEMode = 1,
+		@MPEPolicyID = -84,
+		@IsVirtual = 0,
+		@RecordStatus = 1,
+		@LogMartMode = 13627388,
+		@UDLAConnectionString = '',
+		@UDLAStateField = '',
+		@UDLAStateFieldType = 0,
+		@UDLAStateFieldConversion = '',
+		@UDLAQueryStatement = '',
+		@UDLAOutputFormat = '',
+		@UDLAUniqueIdentifier = '',
+		@UDLAMsgDateField = '',
+		@UDLAGetUTCDateStatement = '',
+		@PersistentConnection = 0,
+		@MaxLogDate = '1900-01-01 00:00:00.000',
+		@Status = 1,
+		@MsgRegexStart = N'^\d{4}-\d{2}-\d{2}',
+		@MsgRegexDelimeter = '',
+		@MsgRegexEnd = '',
+		@RecursionDepth = 0,
+		@IsDirectory = 1,
+		@Inclusions = N'*.log',
+		@Exclusions = '',
+		@Parameter1 = 0,
+		@Parameter2 = 0,
+		@Parameter3 = 0,
+		@Parameter4 = 0,
+		@StatePosition = NULL,
+		@StateLastUpdated = 0,
+		@CompressionType = 0,
+		@UDLAConnectionType = 0,
+		@CollectionThreadTimeout = 120,
+        @MsgSourceID = @MsgSourceID OUTPUT
+        SELECT	@MsgSourceID as N'@MsgSourceID', @return_value as N'@return_value'"
+    Invoke-Sqlcmd -ServerInstance 'localhost' -Database LogRhythmEMDB -Query $Query
+}
+
+
---
+++ IIS_Log_Share.ps1
@@ -0,0 +1,54 @@
+﻿function Set-FolderPermission{
+    param(
+        [string]$folderPath = 'C:\inetpub\logs\LogFiles',
+        [string]$user = "arcci\soc"
+    )
+    $Rights = "Read, ReadAndExecute, ListDirectory"
+    $InheritSettings = "Containerinherit, ObjectInherit"
+    $PropogationSettings = "none"
+    $RuleType = "Allow"
+
+    $acl = Get-Acl $folderPath
+    $perm = $user, $Rights, $InheritSettings, $PropogationSettings, $RuleType
+    $rule = New-Object -TypeName System.Security.AccessControl.FileSystemAccessRule -ArgumentList $perm
+    $acl.SetAccessRule($rule)
+    $acl | Set-Acl -Path $folderPath
+}
+
+function Create-IISShare{
+    param(
+        [string]$logsFilePath = 'C:\inetpub\logs\LogFiles',
+        [string]$User = 'arcci\soc',
+        [string]$name
+    )
+    $Share = New-SmbShare -Path $logsFilePath -Name $name  -ReadAccess $User
+    $Share.Name
+}
+
+
+[string]$OutputFile = "$env:USERPROFILE\Desktop\$($env:ComputerName).txt"
+
+if((Get-WindowsFeature -Name Web-Server).Installed){
+    If((Get-Service -Name w3svc).Status -eq 'Running'){
+        Import-Module WebAdministration
+        $Sites = Get-ChildItem iis:\\Sites
+        Foreach($Site in $Sites){
+            if($Site.State -eq 'Started'){
+                $logFolder = "$($Site.logFile.directory)\w3svc$($Site.id)".replace("%SystemDrive%",$env:SystemDrive)
+                if(!(Test-Path $logFolder)){
+                    New-Item -Path $logFolder -ItemType Directory -Force
+                }
+                $shareName = "w3svc$($Site.id)"
+                Set-FolderPermission -folderPath $logFolder
+                $sName = Create-IISShare -logsFilePath $logFolder -User 'arcci\soc' -name $shareName
+                "$($env:ComputerName),$($Site.Name),\\$($env:ComputerName)\$($sName)"| Out-File -FilePath $OutputFile -Append
+            }
+        }
+    }
+    else{
+        "IIS Installed but service stopped" | sc $OutputFile -Force
+    }
+}
+else{
+    "IIS Not Installed" | sc $OutputFile -Force
+}
